a091d868b2a122fa4e1e22ba5c41a2c904f3ab7e
Ananya <ananyac@andrew.cmu.edu>
2018-05-01 18:55:45 -0400

    updating normalized values in knn

--- run_knn.py
+++ run_knn.py
@@ -1,7 +1,7 @@
 from sklearn import svm
 import os, math, sys, numpy, csv
 
-NUMBER_NEIGHBORS=2
+NUMBER_NEIGHBORS=4
 
 def parseData(inputFile):
 	with open(inputFile, 'rb') as csvfile:
@@ -39,7 +39,7 @@ def predict(X, Y, predict1, predict2, predict3, predict4, predict5, predict6, in
 	knn = dict()
 	for i in xrange(0, len(X)):
 		temp = X[i]	
-		distance = math.sqrt(math.pow((float(temp[0])-predict1)/60.0,2) + math.pow((float(temp[1])-predict2)/2,2) + math.pow((float(temp[2])-predict3)/31000,2) + math.pow((float(temp[3])-predict4)/4,2) + math.pow((float(temp[4])-predict5),2) + math.pow((float(temp[5])-predict6),2))
+		distance = math.sqrt(math.pow((float(temp[0])-predict1)/20.0,2) + math.pow((float(temp[1])-predict2)/100,2) + math.pow((float(temp[2])-predict3)/5000,2) + math.pow((float(temp[3])-predict4)/20,2) + math.pow((float(temp[4])-predict5)/15,2) + math.pow((float(temp[5])-predict6)/600,2))
 		if len(knn) < NUMBER_NEIGHBORS:
 			knn[distance] = i 
 		else:
@@ -50,7 +50,6 @@ def predict(X, Y, predict1, predict2, predict3, predict4, predict5, predict6, in
 			if distance < maxDistance:
 				knn.pop(maxDistance)
 				knn[distance] = i
-			print "here"
 
 	zeroCount = 0
 	oneCount = 0
@@ -83,15 +82,15 @@ def run_knn(inputData, new_data):
 	X, Y, labels, indeces = parseData(inputData)
 	predict(X, Y, float(predict1), float(predict2), float(predict3), float(predict4), float(predict5), float(predict6), indeces)
 
-if __name__ == '__main__':
-	inputData = sys.argv[1]
-	predict1 = sys.argv[2]
-	predict2 = sys.argv[3]
-	predict3 = sys.argv[4]
-	predict4 = sys.argv[5]
-	predict5 = sys.argv[6]
-	predict6 = sys.argv[7]
+# if __name__ == '__main__':
+# 	inputData = sys.argv[1]
+# 	predict1 = sys.argv[2]
+# 	predict2 = sys.argv[3]
+# 	predict3 = sys.argv[4]
+# 	predict4 = sys.argv[5]
+# 	predict5 = sys.argv[6]
+# 	predict6 = sys.argv[7]
 
-	X, Y, labels, indeces = parseData(inputData)
-	predict(X, Y, float(predict1), float(predict2), float(predict3), float(predict4), float(predict5), float(predict6), indeces)
+# 	X, Y, labels, indeces = parseData(inputData)
+# 	predict(X, Y, float(predict1), float(predict2), float(predict3), float(predict4), float(predict5), float(predict6), indeces)
 
